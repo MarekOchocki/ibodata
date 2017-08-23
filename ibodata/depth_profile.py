1786cd8565a83262d3672bd41985ed424a8a0dda
MarekOchocki <marcopolo97@vp.pl>
2017-08-23 16:49:12 +0200

    Add 4 methods in depth_profile
    - normalize
    - range
    - distal_falloff
    - normalize

--- ibodata/depth_profile.py
+++ ibodata/depth_profile.py
@@ -2,5 +2,25 @@ from beprof.profile import Profile
 
 
 class DepthProfile(Profile):
-    """TODO"""
-    pass
+
+    def range(self, level):
+        return self.width(level)
+
+    def FWHM(self):
+        return self.width(0.5)
+
+    def distal_falloff(self):
+        return self.x_at_y(0.1, True) - self.x_at_y(0.9, True)
+
+    def normalize(self, allow_cast=True):
+        """
+        Scale and translate y to be in range(0,1)
+        Normalize before calling other methods
+        """
+        self.y -= self.y.min()
+
+        if allow_cast:
+            self.y = self.y / self.y.max()
+        else:
+            self.y /= self.y.max()
+
